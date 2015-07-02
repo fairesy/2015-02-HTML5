874758a2fbacc26d1a05dc7ede32b86fff6f7b4a
Shinyoung <fairesy@gmail.com>
2015-07-02 20:43:17 +0900

    01_3 : 함수분리

--- asset/todolist.js
+++ asset/todolist.js
@@ -5,42 +5,31 @@
  */
 
 document.addEventListener("DOMContentLoaded", function(){
-
 	var newTodo = document.getElementById("new-todo");
-	newTodo.addEventListener("keydown", function(event){
-		// 1 enter키 입력 
-		if(event.keyCode === 13){
-			// 2 todo 추가 : li 생성(구조에 맞춰서) - List에 li 추가  
-			var todoLabel = newTodo.value;
-			if(todoLabel === ""){
-				alert("할 일을 입력해주세요!");
-				return;
-			}
-
-			// var todoItem = document.createElement("li");
-			// todoItem.setAttribute("class", "{}");
-			// todoItem.innerHTML = '<div class="view"><input class="toggle" type="checkbox" {}><label>' 
-			// 					+ todoLabel 
-			// 					+ '</label><button class="destroy"></button></div>';
-			// var todoList = document.getElementById("todo-list");
-			// todoList.appendChild(todoItem);
-
-			// handlebar 적용
-			// [TODO] li까지 template으로 - jquery parseHTML()?
-			var todoItem = document.createElement("li");
-			todoItem.setAttribute("class", "{}");
-			var source = '<div class="view"><input class="toggle" type="checkbox" {}><label>{{todoLabel}}</label><button class="destroy"></button></div>';
-			var template = Handlebars.compile(source);
-			var data = {"todoLabel" : todoLabel};
-			var result = template(data);
-			
-			todoItem.innerHTML = result;
-			var todoList = document.getElementById("todo-list");
-			todoList.appendChild(todoItem);
-
-			// input창 초기화
-			newTodo.value = "";
-		}		
-	}, false);
-
+	newTodo.addEventListener("keydown", AddTodoItemToList, false);
 });
+
+function AddTodoItemToList(event){
+	if(event.keyCode === 13){
+		var newTodo = document.getElementById("new-todo");
+		var todoLabel = newTodo.value;
+		if(todoLabel === ""){
+			alert("할 일을 입력해주세요!");
+			return;
+		}
+		document.getElementById("todo-list").appendChild(createTodoItem(todoLabel));
+		newTodo.value = "";
+	}
+}
+
+function createTodoItem(todoLabel){
+	var todoItem = document.createElement("li");
+	todoItem.setAttribute("class", "{}");
+	var source = '<div class="view"><input class="toggle" type="checkbox" {}><label>{{todoLabel}}</label><button class="destroy"></button></div>';
+	var template = Handlebars.compile(source);
+	var data = {"todoLabel" : todoLabel};
+	var result = template(data);
+	todoItem.innerHTML = result;
+
+	return todoItem;
+}
